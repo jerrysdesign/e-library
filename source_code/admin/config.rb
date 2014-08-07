9475e97628adc50e45f0d5e6f4a570be2952c813
jerrysdesign <jerry7722@hotmail.com>
2014-08-07 09:59:24 +0800

    irs移除已轉移至 /soure_core/admin/ 原watch /soure_core/ 故sass檔已轉移 改watch為 /soure_core/admin/

--- source_code/admin/config.rb
+++ source_code/admin/config.rb
@@ -30,6 +30,17 @@ line_comments = false
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
 
+css_dir = "css" # by Fire.app 
+sass_dir = "sass" # by Fire.app 
+images_dir = "imgges" # by Fire.app 
+javascripts_dir = "script" # by Fire.app 
+fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
+fireapp_livescripts_dir = "livescripts" # by Fire.app 
+fireapp_minifyjs_on_build = false # by Fire.app 
+fireapp_always_report_on_build = true # by Fire.app 
+output_style = :compact # by Fire.app 
+relative_assets = true # by Fire.app 
+line_comments = false # by Fire.app 
 sass_options = {:debug_info=>false} # by Fire.app 
 fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
 fireapp_livescript_options = {:bare=>false} # by Fire.app 
